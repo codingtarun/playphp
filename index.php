706743eaa154561a7ab9c9e2c004c5ba0b08fb69
Tarun Chauhan <coding.tarun@gmail.com>
2024-08-28 20:57:42 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * 
+ * Variables : Container that holds data , whcih can be accessed later.
+ * Rules for declaring variables : 
+ * 1. Prefix with a $ sign.
+ * 2. Must start with a letter or an underscore.
+ * 3. Can not start with a number.
+ * 4. Can only contain letters, numbers and underscores.
+ * 5. are case senstive i.e $Name is not equal to $name.
+ *  
+ * 
+ * Naming convention : 
+ * 1. $under_score : preferred internaly by developing PHP.
+ * 2. $camelCase : preffered
+ * 3. $PascalCase
+ * 4. $lowecase
+ */
+
+$title = "PHP : PHP Hypertext Preprocessor"; // declaring & storing value to variable.
+
+/**
+ * Datatypes : PHP is a loosly typed langauge i.e we don't need to define data type.
+ * Data types supported by the PHP are : 
+ * 1. String :  Sequence of characters.
+ * 2. Integer : Whole Number. It can be positive or negitive.
+ * 3. Float : Number with a decimal point.
+ * 4. Boolean : true/false.
+ * 5. Array : Holds multiple values.
+ * 6. Object : Collection of properties & methods.
+ * 7. null : Variable with no value.
+ * 8. Resource : Files stored in the server. $file = fopen('fileName.txt','r');
+ * 
+ * 
+ */
+
+
+
+
+
+var_dump($title); // dumps the data & data type information to the screen.
+echo "<br>";
+echo gettype($title); // display the data type and one of few function in PHP which uses camelCase.
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title><?php echo $title; ?></title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <h1><?php echo "PHP Basics"; ?></h1>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
