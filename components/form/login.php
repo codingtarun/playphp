022f0e557b15b18b92d81bb7466e5afa1c0113f3
Tarun Chauhan <coding.tarun@gmail.com>
2024-09-20 12:13:59 +0530

    Login Input Validation

--- components/form/login.php
+++ components/form/login.php
@@ -5,15 +5,21 @@
             <form method="POST" action="<?php echo '../controller/login.php' ?>">
                 <div class="mb-3">
                     <label for="userEmail" class="form-label">Email</label>
-                    <input type="email" class="form-control is-invalid" name="userEmail" id="userEmail">
-                    <div class="invalid-feedback">Enter correct email address</div>
+                    <input type="text" class="form-control <?php if (isset($_SESSION['errors']['userEmail'])) { ?> is-invalid <?php } ?>" name="userEmail" id="userEmail">
+                    <?php if (isset($_SESSION['errors']['userEmail'])) { ?>
+                        <div class="invalid-feedback"><?php echo $_SESSION['errors']['userEmail']; ?></div>
+                    <?php } ?>
                 </div>
                 <div class="mb-3">
                     <label for="userPassword" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="confirmPassword" id="userPassword">
+                    <input type="password" class="form-control <?php if (isset($_SESSION['errors']['userPassword'])) { ?>is-invalid <?php } ?>" name="userPassword" id="userPassword">
+                    <?php if (isset($_SESSION['errors']['userPassword'])) { ?>
+                        <div class="invalid-feedback"><?php echo $_SESSION['errors']['userPassword']; ?></div>
+                    <?php } ?>
                 </div>
                 <button type="submit" class="btn btn-primary">Login</button>
             </form>
+            <?php unset($_SESSION['errors']); ?>
         </div>
     </div>
 </div>
--- user/controller/login.php
+++ user/controller/login.php
@@ -14,3 +14,60 @@ if (isset($_SESSION['user'])) {
 /**
  * Run validation for input 
  */
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    /**
+     * Request is coming from the login page
+     */
+    $errors = [];
+    $alerts = [];
+    /**
+     * Validating the data 
+     */
+    if (!empty($_POST['userEmail'])) {
+        /**
+         * Perform validations
+         */
+        $userEmail = htmlspecialchars(trim($_POST['userEmail']));
+        $userEmailLenght = strlen($userEmail);
+        if ($userEmailLenght > 8) {
+            if (filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
+                if (preg_match("/^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $userEmail)) {
+                    /**
+                     * All validations are successfull now check DB for given credentials
+                     */
+                    echo "OK";
+                } else {
+                    $errors['userEmail'] = "Email can only contain letters, numbers, and the special characters _ and . ";
+                }
+            } else {
+                $errors['userEmail'] = "Incorrect email format";
+            }
+        } else {
+            $errors['userEmail'] = "Email must be atlest 8 character long";
+        }
+    } else {
+        /**
+         * Return error of empty email field
+         */
+        $errors['userEmail'] = "Email field cannot be blank";
+    }
+    if (!empty($_POST['userPassword'])) {
+        /**
+         * Perform Validations
+         */
+    } else {
+        /**
+         * Return error for empty pasword field
+         */
+        $errors['userPassword'] = "Password filed cannot be blank";
+    }
+
+    if (!empty($errors)) {
+        $_SESSION['errors'] = $errors;
+        header("Location: http://127.0.0.1:8000/user/view/login.php");
+        exit();
+    }
+} else {
+    header("Location:http://127.0.0.1:8000");
+    exit;
+}
