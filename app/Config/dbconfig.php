25469979748d05b7e92b7e2b23b29e95a1c5a175
Tarun Chauhan <coding.tarun@gmail.com>
2024-09-25 17:17:47 +0530

    RESET PROJECT

--- app/Config/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'bs09Btcs183@');
-define('DB_NAME', 'playphp');
--- index.php
+++ index.php
@@ -10,29 +10,7 @@
 
 <body>
     <h1>PHP OOP</h1>
-    <?php
-    /**
-     * Including files
-     */
-    include_once('./app/Controllers/User.php');
-    /**
-     * Creating object of the User class
-     * 1. Object is used to access properties and methods of the class
-     * 2. Objects are also called as instance of the class
-     * 3. We can create as many as we required objects of class.
-     * 4. Each instance of the object is isolated and has its own set of data.
-     */
-    $objUser = new User(); // creating object
-    $objUser->create(); // access method 'create()'
 
-
-    /**
-     * Files class object
-     */
-    include_once('./app/Controllers/File.php');
-    $objFile = new File();
-    echo $objFile->displayContent('./.env');
-    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
