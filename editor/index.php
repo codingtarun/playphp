f7c4e0553d77146c9f1abed6c89ecd697b3fe73f
coding.tarun <coding.tarun@gmail.com>
2024-10-12 22:16:13 +0530

    Editor module begin

---
+++ editor/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editor</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body class="app">
+    <header class="header">
+        <img src="https://res.cloudinary.com/dx6ecncex/image/upload/v1620757017/Group_13_uyn5ja.svg" alt="logo" class="logo">
+        <button class="btn-toggle">File</button>
+        <button class="btn-toggle">View</button>
+        <button class="btn-toggle">New</button>
+        <button class="btn-toggle">
+            <i class="fa-solid fa-crown btn-toggle__icon"></i>
+        </button>
+        <button class="btn-toggle">
+            <i class="fa-solid fa-bell btn-toggle__icon"></i>
+        </button>
+        <button class="btn-toggle">
+            <img src="https://imageplaceholder.net/50x50" alt="user" class="btn-toggle__user-image">
+            <span class="btn-toggle__user-name">Tarun C.</span>
+        </button>
+    </header>
+</body>
+
+</html>
