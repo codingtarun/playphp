d4a540014afd6698b1a3def0284f20724b5563a4
coding.tarun <coding.tarun@gmail.com>
2024-11-25 20:37:49 +0530

    Inheritance topic begin

--- oop/Bank.php
+++ oop/Bank.php
@@ -1,26 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 class Bank
 {
 
     /**
      *
-     *  PHP Method Chaining is a programming technique where multiple methods are called on the same object in a single line. 
+     * PHP Method Chaining is a programming technique where multiple methods are called on the same object in a single line. 
      * It works by having each method return the object itself, allowing you to chain multiple method calls together.
      * 
-     *  How It Works: 
+     * How It Works: 
      * 1. Each method in the chain must return the current object ($this).
      * 2. This allows subsequent method calls to operate on the same object.
      *
      */
 
 
-    private $balance; // Can only be accesse onside the function
-    private $name; // Can be accesses outside of the class
-    private $bankName;
+    private $balance;
+    private $name;
+    public $bankName;
     public function __construct($bankName)
     {
         $this->bankName = $bankName;
+        echo "<br>PARENT CLASS</br>";
     }
 
     /**
@@ -65,10 +68,42 @@ class Bank
     }
 }
 
-
-$objBank = new Bank("HDFC Kotkhai", 100000);
+echo "PARENT CLASS<br>";
+$objBank = new Bank("HDFC Kotkhai");
 $objBank->setBalance(10000);
 $objBank->setName("Tarun Chauhan");
 $objBank->checkBalance();
 $objBank->debitMoney(20000)->checkBalance();
 $objBank->creditMoney(50000)->checkBalance();
+echo "<hr>";
+/**
+ * 
+ * Inheritance : A PHP feature which alows one class(child class) to inherit the properties & methods of another class(Parent class).
+ * 
+ */
+class Salary extends Bank
+{
+    /**
+     * This is child class
+     */
+    private $salaryDate;
+
+    public function __construct(string $salaryDate) // Both classes have constructor , which will be called and what argument to pass
+    {
+        $this->salaryDate = $salaryDate;
+    }
+    public function salaryDate()
+    {
+        echo "Salary will be creditied on $this->salaryDate of every month<br>";
+    }
+}
+echo "CHILD CLASS";
+$objSalary = new Salary("Co-oprative bank"); // since parent class has a construct and requires some argument , so child also need arguments by default.
+$objSalary->setBalance(20000); // using methods of parent class
+$objSalary->checkBalance(); // using child object to call parent function.
+echo $objSalary->bankName . "<br>"; // Accessing a parent class property in child class object.
+$objSalary->salaryDate(); // Child class method
+
+
+
+echo "<br>";
